cb636d0c18ca81c157b99d0cea666f268f4c1c51
PabloRodriguezLomba <105041033+PabloRodriguezLomba@users.noreply.github.com>
2022-11-04 11:22:00 +0100

    Script de creacion de la base de datos finalizado, creo?

--- creacionBBDD.sql
+++ creacionBBDD.sql
@@ -20,3 +20,81 @@ CREATE TABLE headgear_skills (
         ON DELETE CASCADE,
     PRIMARY KEY(headgear, skill)
 ) ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS Chest (
+    id INT UNSIGNED NOT NULL auto_increment,
+    name VARCHAR(100) NOT NULL,
+    fire_res INT NOT NULL,
+    water_res INT NOT NULL,
+    ice_res INT NOT NULL,
+    thunder_res INT NOT NULL,
+    dragon_res INT NOT NULL,
+    PRIMARY KEY(id)
+)ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS Chest_skills (
+    Chest INT UNSIGNED NOT NULL,
+    skill VARCHAR(100) NOT NULL,
+    skill_level int not null,
+    FOREIGN KEY(Chest) REFERENCES Chest(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    PRIMARY KEY(Chest,skill)
+
+) ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS gloves (
+    id INT UNSIGNED NOT NULL auto_increment,
+    name VARCHAR(100) NOT NULL,
+    fire_res INT NOT NULL,
+    water_res INT NOT NULL,
+    ice_res INT NOT NULL,
+    thunder_res INT NOT NULL,
+    dragon_res INT NOT NULL,
+    PRIMARY KEY(id)
+)ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS gloves_skills (
+    gloves INT UNSIGNED NOT NULL,
+    skill VARCHAR(100) NOT NULL,
+    skill_level int not null,
+    FOREIGN KEY(gloves) REFERENCES gloves(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    PRIMARY KEY(gloves,skill)
+)ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS waist (
+    id INT UNSIGNED NOT NULL auto_increment,
+    name VARCHAR(100) NOT NULL,
+    fire_res INT NOT NULL,
+    water_res INT NOT NULL,
+    ice_res INT NOT NULL,
+    thunder_res INT NOT NULL,
+    dragon_res INT NOT NULL,
+    PRIMARY KEY(id)
+)ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS waist_skills (
+    waist INT UNSIGNED NOT NULL,
+    skill VARCHAR(100) NOT NULL,
+    skill_level int not null,
+    FOREIGN KEY(waist) REFERENCES waist(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    PRIMARY KEY(waist,skill)
+) ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS legs (
+    id INT UNSIGNED NOT NULL auto_increment,
+    name VARCHAR(100) NOT NULL,
+    fire_res INT NOT NULL,
+    water_res INT NOT NULL,
+    ice_res INT NOT NULL,
+    thunder_res INT NOT NULL,
+    dragon_res INT NOT NULL,
+    PRIMARY KEY(id)
+
+)ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS legs_skills (
+    legs INT UNSIGNED NOT NULL,
+    skill VARCHAR(100) NOT NULL,
+    skill_level int not null,
+    FOREIGN KEY(legs) REFERENCES legs(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    PRIMARY KEY (legs,skill)
+)ENGINE=Innodb;
